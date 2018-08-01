9e10aea5feb3de39070fc99d41fe6389d6794370
Kamsi Mbanefo <Kamsimbanefo@yahoo.com>
2018-08-01 12:22:45 +0100

    Made Checkers Pieces

--- CheckersGUI/Form1.cs
+++ CheckersGUI/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Checkers.DataFixture;
+using Checkers.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,10 +16,15 @@ namespace CheckersGUI
     {
         private Pen blackPen = new Pen(Color.Black);
         private Brush whiteBrush = new SolidBrush(Color.White);
+        private Brush greyBrush = new SolidBrush(Color.Gray);
+        private Brush redBrush = new SolidBrush(Color.Red);
         private Brush blackBrush = new SolidBrush(Color.Black);
+        private Brush yellowBrush = new SolidBrush(Color.Yellow);
         const int squareSize = 40;
         const int boardSize = 8;
         Graphics g;
+        private GameBoard Board;
+
 
         public Form1()
         {
@@ -27,11 +34,19 @@ namespace CheckersGUI
 
         private void Start1PGame_Click(object sender, EventArgs e)
         {
+            var blackpieces = Pieces.BlackPlacements();
+            var whitepieces = Pieces.WhitePlacements();
+            Board = new GameBoard(8, blackpieces, whitepieces);
+            Initialize1PGame(Board);
             DrawBoard();
         }
 
         private void Start2PGame_Click(object sender, EventArgs e)
         {
+            var blackpieces = Pieces.BlackPlacements();
+            var whitepieces = Pieces.WhitePlacements();
+            Board = new GameBoard(8, blackpieces, whitepieces);
+            Initialize2PGame(Board);
             DrawBoard();
         }
 
@@ -40,12 +55,24 @@ namespace CheckersGUI
             Application.Exit();
         }
 
+        private void Initialize1PGame(GameBoard board)
+        {
+            Board.InitializePieces();
+        }
+
+        private void Initialize2PGame(GameBoard board)
+        {
+            Board.InitializePieces();
+        }
+
         private void DrawBoard()
         {
             for (int row = 0; row < 8; row++)
             {
                 for (int col = 0; col < 8; col++)
                 {
+                    var square = Board.ReadSquare(col,row);
+
                     Brush brush = whiteBrush;
                     if (row % 2 == 0 && col % 2 == 0)
                     {
@@ -54,7 +81,7 @@ namespace CheckersGUI
 
                     if (row % 2 == 0 && col % 2 == 1)
                     {
-                        DrawSquare(col, row, blackPen, blackBrush);
+                        DrawSquare(col, row, blackPen, greyBrush);
                     }
 
                     if (row % 2 == 1 && col % 2 == 1)
@@ -64,9 +91,28 @@ namespace CheckersGUI
 
                     if (row % 2 == 1 && col % 2 == 0)
                     {
-                        DrawSquare(col, row, blackPen, blackBrush);
+                        DrawSquare(col, row, blackPen, greyBrush);
                     }
+                    switch (square)
+                    {
+                        case SquareValues.Empty:
+                            break;
+                        case SquareValues.Black:
+                            DrawCircle(col, row, blackPen, blackBrush);
+                            break;
+                        case SquareValues.BlackKing:
+                            DrawCircle(col, row, blackPen, blackBrush);
+                            DrawInnerSquare(col, row, blackPen, yellowBrush);
+                            break;
+                        case SquareValues.White:
+                            DrawCircle(col, row, blackPen, whiteBrush);
+                            break;
+                        case SquareValues.WhiteKing:
+                            DrawCircle(col, row, blackPen, whiteBrush);
+                            DrawInnerSquare(col, row, blackPen, yellowBrush);
+                            break;
 
+                    }
                 }
             }
 
@@ -81,6 +127,45 @@ namespace CheckersGUI
             }
         }
 
-        
+        private void DrawCircle(int col, int row, Pen pen, Brush fill)
+        {
+            g.DrawEllipse(pen, col * squareSize, row * squareSize, squareSize, squareSize);
+            if (fill != null)
+            {
+                g.FillEllipse(fill, col * squareSize + 1, row * squareSize + 1, squareSize - 1, squareSize - 1);
+            }
+        }
+
+        private void DrawStar(Pen pen, Brush fill)
+        {
+            g.DrawPolygon(pen,Starpoints());
+            if (fill != null)
+            {
+                g.FillPolygon(fill, Starpoints());
+            }
+        }
+
+        private Point[] Starpoints()
+        {
+            var p = new Point[8];
+            p[0] = new Point(0, 25);
+            p[1] = new Point(20, 20);
+            p[2] = new Point(25, 0);
+            p[3] = new Point(30, 20);
+            p[4] = new Point(50, 25);
+            p[5] = new Point(30, 30);
+            p[6] = new Point(25, 50);
+            p[7] = new Point(20, 30);
+            return p;
+        }
+
+        private void DrawInnerSquare(int col, int row, Pen pen, Brush fill)
+        {
+            g.DrawRectangle(pen, col * squareSize+10, row * squareSize+10, squareSize - 20, squareSize - 20);
+            if (fill != null)
+            {
+                g.FillRectangle(fill, col * squareSize + 11, row * squareSize + 11, squareSize - 21, squareSize - 21);
+            }
+        }
     }
 }
