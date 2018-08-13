bff0bb898f6ecef79def8db120f0cd04d9ac773d
Kamsi Mbanefo <Kamsimbanefo@yahoo.com>
2018-08-13 14:06:38 +0100

    Test Settings reverted

--- CheckersGUI/Form1.cs
+++ CheckersGUI/Form1.cs
@@ -47,10 +47,10 @@ namespace CheckersGUI
             InitializeComponent();
             g = Grid.CreateGraphics();
             Mode = Modality.BlackTurn;
-            //var blackpieces = Pieces.BlackPlacements();
-            //var whitepieces = Pieces.WhitePlacements();
-            var blackpieces = Pieces.TestBlack();
-            var whitepieces = Pieces.DoubleJumpWhite();
+            var blackpieces = Pieces.BlackPlacements();
+            var whitepieces = Pieces.WhitePlacements();
+            //var blackpieces = Pieces.TestBlack();
+            //var whitepieces = Pieces.DoubleJumpWhite();
             Board = new GameBoard(8, blackpieces, whitepieces);
             Messages.Text = "WELCOME TO CHECKERS" +
                 " \nClick the Game tab on the top left to begin";
