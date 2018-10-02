03de3fd6afb7c0dfac81b2f7c77184aa11a9c01a
Kamsi Mbanefo <Kamsimbanefo@yahoo.com>
2018-10-02 14:44:50 +0100

    Made unit tests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Checkers.DataFixture;
+using Checkers.Model;
+using Checkers.UI;
+using Checkers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
