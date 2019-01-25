3f3e9bdd0b9f00731e8be91912a67d8d25b80535
Kamsi Mbanefo <kmbanefo2014@gmail.com>
2019-01-25 11:51:02 +0000

    Took out fast forward and reverse elements

--- CheckersGUI/Form1.Designer.cs
+++ CheckersGUI/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.MenuGame = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.endGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.CLIversion = new System.Windows.Forms.ToolStripMenuItem();
@@ -57,10 +59,6 @@
             this.BlackPiecePic = new System.Windows.Forms.PictureBox();
             this.WhitePiecePic = new System.Windows.Forms.PictureBox();
             this.PlayPause = new System.Windows.Forms.Button();
-            this.Reverse = new System.Windows.Forms.Button();
-            this.FastForward = new System.Windows.Forms.Button();
-            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.loadGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.BlackPiecePic)).BeginInit();
@@ -113,38 +111,52 @@
             // MenuNewGame
             // 
             this.MenuNewGame.Name = "MenuNewGame";
-            this.MenuNewGame.Size = new System.Drawing.Size(180, 22);
+            this.MenuNewGame.Size = new System.Drawing.Size(134, 22);
             this.MenuNewGame.Text = "&New Game";
             this.MenuNewGame.Click += new System.EventHandler(this.MenuNewGame_Click);
+            // 
+            // saveGameToolStripMenuItem
+            // 
+            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
+            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.saveGameToolStripMenuItem.Text = "Save Game";
+            this.saveGameToolStripMenuItem.Click += new System.EventHandler(this.saveGameToolStripMenuItem_Click);
+            // 
+            // loadGameToolStripMenuItem
+            // 
+            this.loadGameToolStripMenuItem.Name = "loadGameToolStripMenuItem";
+            this.loadGameToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.loadGameToolStripMenuItem.Text = "Load Game";
+            this.loadGameToolStripMenuItem.Click += new System.EventHandler(this.loadGameToolStripMenuItem_Click);
             // 
             // endGameToolStripMenuItem
             // 
             this.endGameToolStripMenuItem.Name = "endGameToolStripMenuItem";
-            this.endGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.endGameToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.endGameToolStripMenuItem.Text = "&End Game";
             this.endGameToolStripMenuItem.Click += new System.EventHandler(this.endGameToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(131, 6);
             // 
             // CLIversion
             // 
             this.CLIversion.Name = "CLIversion";
-            this.CLIversion.Size = new System.Drawing.Size(180, 22);
+            this.CLIversion.Size = new System.Drawing.Size(134, 22);
             this.CLIversion.Text = "&CLI Version";
             this.CLIversion.Click += new System.EventHandler(this.CLIversion_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(131, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.exitToolStripMenuItem.Text = "&Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -311,51 +323,11 @@
             this.PlayPause.Visible = false;
             this.PlayPause.Click += new System.EventHandler(this.PlayPause_Click);
             // 
-            // Reverse
-            // 
-            this.Reverse.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.Reverse.Image = global::CheckersGUI.Properties.Resources.Rewind;
-            this.Reverse.Location = new System.Drawing.Point(443, 390);
-            this.Reverse.Name = "Reverse";
-            this.Reverse.Size = new System.Drawing.Size(39, 31);
-            this.Reverse.TabIndex = 22;
-            this.Reverse.UseVisualStyleBackColor = true;
-            this.Reverse.Visible = false;
-            this.Reverse.Click += new System.EventHandler(this.Reverse_Click);
-            // 
-            // FastForward
-            // 
-            this.FastForward.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.FastForward.Image = global::CheckersGUI.Properties.Resources.FastForward;
-            this.FastForward.Location = new System.Drawing.Point(533, 390);
-            this.FastForward.Name = "FastForward";
-            this.FastForward.Size = new System.Drawing.Size(39, 31);
-            this.FastForward.TabIndex = 23;
-            this.FastForward.UseVisualStyleBackColor = true;
-            this.FastForward.Visible = false;
-            this.FastForward.Click += new System.EventHandler(this.FastForward_Click);
-            // 
-            // saveGameToolStripMenuItem
-            // 
-            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
-            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.saveGameToolStripMenuItem.Text = "Save Game";
-            this.saveGameToolStripMenuItem.Click += new System.EventHandler(this.saveGameToolStripMenuItem_Click);
-            // 
-            // loadGameToolStripMenuItem
-            // 
-            this.loadGameToolStripMenuItem.Name = "loadGameToolStripMenuItem";
-            this.loadGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.loadGameToolStripMenuItem.Text = "Load Game";
-            this.loadGameToolStripMenuItem.Click += new System.EventHandler(this.loadGameToolStripMenuItem_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 461);
-            this.Controls.Add(this.FastForward);
-            this.Controls.Add(this.Reverse);
             this.Controls.Add(this.PlayPause);
             this.Controls.Add(this.WhitePiecePic);
             this.Controls.Add(this.BlackPiecePic);
@@ -415,8 +387,6 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem CLIversion;
         private System.Windows.Forms.Button PlayPause;
-        private System.Windows.Forms.Button Reverse;
-        private System.Windows.Forms.Button FastForward;
         private System.Windows.Forms.ToolStripMenuItem saveGameToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadGameToolStripMenuItem;
     }
